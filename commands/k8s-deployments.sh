ea80e1f83f9225772987cc35f5064122ab0888af
paulopez78 <pau.lopez.78@gmail.com>
2020-02-26 23:34:06 +0100

    rolling update and blue green with one pod samples

--- commands/k8s-deployments.sh
+++ commands/k8s-deployments.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# rolling update with one pod
+kubectl run votingapp --image=paulopez/votingapp:0.1-alpine --generator=run-pod/v1
+kubectl expose pod votingapp --port=8080 --target-port 5000 --type NodePort
+
+kubectl run votingapp-beta --image=paulopez/votingapp:0.2-alpine --generator=run-pod/v1
+kubectl label votingapp-beta run=votingapp --overwrite
+kubectl label votingapp run=votingapp-legacy --overwrite
+
+# rolling update with failing probe
+kubectl apply -f votingapp-pod-readiness-probe.yaml
+
+# get access to the service or pod with port-forward (for troubleshooting)
+kubectl port-forward svc/votingapp 8080:8080
+kubectl port-forward pod/votingapp 8080:5000
+
+# blue green with one pod
+kubectl run votingapp --image=paulopez/votingapp:0.1-alpine --generator=run-pod/v1
+kubectl run votingapp-beta --image=paulopez/votingapp:0.2-alpine --generator=run-pod/v1
+kubectl patch svc votingapp -p '{"spec":{"selector":{"run":"votingapp-beta"}}}'                     
+
+
 # create a deployment yaml resource
 kubectl create deployment nginx --image nginx --dry-run --output yaml
 kubectl run nginx --image nginx --dry-run --expose --port 8080 --output yaml
